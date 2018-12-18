775a6cd81a87550aef06d5cc913e34c30a1371b6
Tomáš Konrády <tomaskonrady@gmail.com>
2018-12-18 14:57:06 +0100

    travis

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,14 +1,15 @@
 const ftp = require('vinyl-ftp');
 const fs = require('vinyl-fs');
 
-const { HOST, PWD } = process.env;
-
-const connection = ftp.create({
-	host: HOST,
-	user: HOST,
-	password: PWD,
+const { PRODUCTION_SERVER_HOST, PRODUCTION_SERVER_PASSWORD } = process.env;
+const options = {
+	host: PRODUCTION_SERVER_HOST,
+	user: PRODUCTION_SERVER_HOST,
+	password: PRODUCTION_SERVER_PASSWORD,
 	parallel: 10,
-});
+};
+const connection = ftp.create(options);
 console.log('Uploading to FTP');
+console.log(options);
 
 fs.src(['./public/**'], { buffer: false }).pipe(connection.dest('/'));
