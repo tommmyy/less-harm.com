031e54cd6d3f7bcda00037911b8f1515fe45fe98
Tomáš Konrády <tomaskonrady@gmail.com>
2018-12-19 00:14:44 +0100

    travis

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,22 +1,34 @@
-const ftp = require('vinyl-ftp');
-const fs = require('vinyl-fs');
+const FtpDeploy = require('ftp-deploy');
 
+const ftpDeploy = new FtpDeploy();
 const { PRODUCTION_SERVER_HOST, PRODUCTION_SERVER_PASSWORD } = process.env;
-const options = {
-	host: PRODUCTION_SERVER_HOST,
+const config = {
 	user: PRODUCTION_SERVER_HOST,
 	password: PRODUCTION_SERVER_PASSWORD,
-	reload: true,
-	log: console.log,
-	debug: console.log,
+	host: PRODUCTION_SERVER_HOST,
+	port: 21,
+	localRoot: `${__dirname}/public`,
+	remoteRoot: '/public/',
+	include: ['*', '**/*'], // this would upload everything except dot files
+	exclude: [], // e.g. exclude sourcemaps - ** exclude: [] if nothing to exclude **
+	deleteRemote: true, // delete existing files at destination before uploading
+	forcePasv: true, // Passive mode is forced (EPSV command is not sent)
 };
-const connection = ftp.create(options);
-console.log('Uploading to FTP');
-console.log(options);
 
-fs.src(['./public/**'], { buffer: true, dot: true })
-	.pipe(connection.dest('/'))
-	.on('end', function() {
-		console.log("We're done!");
+// use with promises
+ftpDeploy
+	.deploy(config)
+	.then(() => 'finished')
+	.then(() => process.exit(0))
+	.catch(err => {
+		console.log(err);
 		process.exit(0);
 	});
+
+// use with callback
+ftpDeploy.deploy(config, function(err) {
+	if (err) console.log(err);
+	else console.log('finished');
+
+	process.exit(0);
+});
